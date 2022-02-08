01f69d9dfbda12316653f93a01f61b0e5bc05f41
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-02-08 09:24:30 -0500

    decode the answer

--- cypress/integration/test-remix-phrasle.js
+++ cypress/integration/test-remix-phrasle.js
@@ -6,34 +6,30 @@ describe(
   'Phrasle',
   { baseUrl: 'https://phrasle.com/', viewportHeight: 800 },
   () => {
-    it.skip('Ross says', () => {
+    it('has the answer', () => {
+      const isVowel = (c) => ['a', 'e', 'i', 'o', 'u'].includes(c)
+      const seenVowels = []
+      const hasSeenVowel = (c) => seenVowels.includes(c)
+
       cy.visit('/')
         .its('__remixContext.routeData.routes/index.answer')
-        .then((answer) => {
-          const letters = answer
-            .split('')
-            .filter((c) => c.match(/[a-z]/))
-          letters.forEach((letter) => {
-            cy.contains('.Key', letter).click()
-          })
-        })
-      cy.contains('.stat-todaysGame-info', 'PERFECT').should(
-        'be.visible',
-      )
-    })
-
-    it('has encoded answer', () => {
-      cy.visit('/')
-        .its('__remixContext.routeData.routes/index')
         .then((s) => atob(s))
         .then(JSON.parse)
-        .its('answer')
+        .then(cy.log)
+        .then((list) => list.map((c) => String.fromCharCode(c - 15)))
+        .then(cy.log)
+        .invoke('join', '')
         .then((answer) => {
           const letters = answer
             .split('')
             .filter((c) => c.match(/[a-z]/))
           letters.forEach((letter) => {
             cy.contains('.Key', letter).click()
+            if (isVowel(letter) && !hasSeenVowel(letter)) {
+              seenVowels.push(letter)
+              cy.contains('button', 'Continue').click()
+              cy.get('[data-testid=overlay]').should('not.exist')
+            }
           })
         })
       cy.contains('.stat-todaysGame-info', 'PERFECT').should(
